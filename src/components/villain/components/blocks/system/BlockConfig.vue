9c359ad438bcd2c3413a077122468b24dc3db71d
T. Mjoen <trond@twined.net>
2020-05-27 12:05:35 +0200

    Add block config component

--- src/components/villain/components/blocks/system/BlockConfig.vue
+++ src/components/villain/components/blocks/system/BlockConfig.vue
@@ -1,13 +1,54 @@
 <template>
-  <div>
-    <slot></slot>
-  </div>
+  <KModal
+    v-if="modalVisible"
+    ref="modal"
+    v-shortkey="['esc']"
+    ok-text="Lukk konfigurasjon"
+    @shortkey.native="closeConfig"
+    @ok="closeConfig">
+    <template #header>
+      <h5>getBlockDisplayName(block.type)</h5>
+    </template>
+    <div
+      ref="config"
+      class="villain-block-config">
+      <div class="villain-block-config-content">
+        <slot v-bind:cfg="innerValue" />
+      </div>
+    </div>
+  </KModal>
 </template>
 
 <script>
 export default {
-  props: ['value']
+  props: ['value'],
 
+  data () {
+    return {
+      modalVisible: false,
+      innerValue: {}
+    }
+  },
+
+  created () {
+    this.innerValue = this.$utils.clone(this.value)
+  },
+
+  updated () {
+    console.debug('<BlockConfig /> updated')
+  },
+
+  methods: {
+    openConfig () {
+      this.modalVisible = true
+    },
+
+    async closeConfig () {
+      await this.$refs.modal.close()
+      this.modalVisible = false
+      this.$emit('input', this.innerValue)
+    }
+  }
 }
 </script>
 
--- src/components/villain/components/blocks/system/index.js
+++ src/components/villain/components/blocks/system/index.js
@@ -1,9 +1,11 @@
 import Block from './Block'
+import BlockConfig from './BlockConfig'
 import BlockContainer from './BlockContainer'
 import TemplateBlock from './TemplateBlock'
 
 export default {
   Block,
+  BlockConfig,
   BlockContainer,
   TemplateBlock
 }
